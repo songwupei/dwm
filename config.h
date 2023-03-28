027d952d3de606b79cf96a917ab9806a16475278
songwupei <songwupei@163.com>
2023-03-28 19:53:32 +0800

    bf rebase dwm6.4

--- config.h
+++ config.h
@@ -4,13 +4,14 @@
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static char font[]            = "Source Code Pro:size=10";
+/*static const char font[]  = "-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1" "," "-*-tamsyn-medium-r-normal-*-12-*-*-*-*-*-*-1";*/
+/* For Iconic Glyphs */                  /* For Normal Text */
 static char dmenufont[]       = "Source Code Pro:size=10";
 /*static char font[]            = "monospace:size=10";*
 *static char dmenufont[]       = "monospace:size=10";*/
@@ -21,25 +22,42 @@ static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
+/* red blue orange green pink  */
+static char col1[]            = "#ffffff";
+static char col2[]            = "#ed333b";
+static char col3[]            = "#71c2ff";
+static char col4[]            = "#ff9458";
+static char col5[]            = "#56e39b";
+static char col6[]            = "#ff628f";
+/*static char col_urgborder[]   = "#ff0000";*/
+
+enum { SchemeNorm, SchemeCol1, SchemeCol2, SchemeCol3, SchemeCol4,
+       SchemeCol5, SchemeCol6, SchemeSel, SchemeUrg }; /* color schemes */
+
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+	[SchemeUrg]  = { selfgcolor,  selbgcolor,  selbordercolor },
+	[SchemeCol1]  = { col1,      normbgcolor, normbordercolor },
+	[SchemeCol2]  = { col2,      normbgcolor, normbordercolor },
+	[SchemeCol3]  = { col3,      normbgcolor, normbordercolor },
+	[SchemeCol4]  = { col4,      normbgcolor, normbordercolor },
+	[SchemeCol5]  = { col5,      normbgcolor, normbordercolor },
+	[SchemeCol6]  = { col6,      normbgcolor, normbordercolor },
 };
 
 static const char *const autostart[] = {
 	"st", NULL,
-	"firefox", NULL,
-	"wps", NULL,
-//	"sh","-c","while :; do ${HOME}/.dwm/dwmrc.sh -;done",NULL,
+/*	"firefox", NULL, */
+/*	"wps", NULL, */
+/*	"sh","-c","sleep 10 && ~/.nutstore/dist/bin/nutstore-pydaemon.py &",NULL,*/
+	"zsh","-c","${HOME}/.config/dwm/dwmrc.zsh",NULL,
 	"sh","-c","while :; do feh --randomize --bg-fill ~/Pictures/wallpaper/*.jpg; sleep 600; done &",NULL,
-	"sh","-c","slstatus&",NULL,
-	"sh","-c","sleep 10 && ~/.nutstore/dist/bin/nutstore-pydaemon.py &",NULL,
-	"sh","-c","sleep 10 && compton &",NULL,
+	/*"sh","-c","slstatus&",NULL,*/
+	"zsh","-c","sleep 10 ; case $(cat /etc/issue | cut -d " " -f1) { (Arch)  picom & ;; (Ubuntu) compton & ;; }",NULL,
 	"sh","-c","sleep 10 && ibus-daemon --xim -d ",NULL,
-	
 	NULL /* terminate */
-			
 };
 
 /* tagging */
@@ -52,7 +70,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "wps",      NULL,       NULL,       1 << 3,       0,           -1 },
 };
 
@@ -89,6 +107,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 static const Key keys[] = {
@@ -101,6 +120,11 @@ static const char *lowervolume[] = {"sh","-c","amixer set Master '3-'",NULL};
 <<<<<<< HEAD
 >>>>>>> 0106633 (bindsym XF86XK_Audio**)
 =======
+=======
+static const char *raisevolume[] = {"sh","-c","pactl set-sink-volume 0 +3%",NULL};
+static const char *mutevolume[] = {"sh","-c"," pactl set-sink-mute 0 toggle ",NULL};
+static const char *lowervolume[] = {"sh","-c","pactl set-sink-volume 0 -3%",NULL};
+>>>>>>> 2633df8 (my dwm)
 static const char *slockcmd[] = { "slock", NULL };
 >>>>>>> d63303a (bindsym win(Mod4Mask)+l slock)
 /*
@@ -174,7 +198,9 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
